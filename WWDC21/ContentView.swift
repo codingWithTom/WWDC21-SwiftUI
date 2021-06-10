4b2be721293c567863efeb828311724456f345a7
Tomas Trujillo <t.trujillo138@gmail.com>
2021-06-10 06:09:43 -0700

    Initial Commit

---
+++ WWDC21/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WWDC21
+//
+//  Created by Tomas Trujillo on 2021-06-10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
